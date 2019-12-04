ff3ae7648f7b0cb9775bc81aa6bdcfe6ca5d7694
Arvid van de Crommert <arvidvdc@gmail.com>
2019-12-04 09:37:49 +0100

    Initial Express-test upload.

---
+++ express-test/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ hope: "loop" });
+});
+
+app.post("/", (req, res) => {
+  const { name } = req.body;
+  if (!name || name === undefined) {
+    res.sendStatus(400);
+  } else {
+    res.json({ input: name });
+  }
+});
+
+// app.listen(3000,()=>console.log("Testapp started"));
+module.exports = app;
---
+++ express-test/test/index-specs.js
@@ -0,0 +1,49 @@
+const   supertest   = require("supertest"),
+        assert      = require('assert'),
+        app         = require("../index");
+
+describe("GET /", function() {
+    it("it should has status code 200", function(done) {
+      supertest(app)
+        .get("/")
+        .expect(200)
+        .end(function(err, res){
+          if (err) done(err);
+          done();
+        });
+    });
+    
+    it("it shoud has response with hope key with value of loop", function(done){
+        supertest(app)
+          .get("/")
+          .expect({ hope: "loop" })
+          .end(function(err, res){
+            if (err) done(err);
+            done();
+          });
+    });
+  });
+
+  describe("POST /", function(){
+    it("it shoud return status code 200 is name exists", function(done) {
+      supertest(app)
+        .post("/")
+        .send({ name: "Hope" })
+        .expect(200)
+        .end(function(err, res){
+          if (err) done(err);
+          done();
+        });
+    });
+
+    it("it shoud return status code 400 if we dosent send anything", function(done){
+        supertest(app)
+          .post("/")
+          .send({})
+          .expect(400)
+          .end(function(err, res){
+            if (err) done(err);
+            done();
+          });
+      });
+  });
